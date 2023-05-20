8764b9e2ba4550e9c245f8c8f07260f5f53fe545
Koniczyn <piotr12konczyk27@gmail.com>
2023-05-20 18:52:42 +0200

    Błąd po deploymencie ;)

--- src/pages/Serie.jsx
+++ src/pages/Serie.jsx
@@ -19,16 +19,16 @@ const Serie = ({ singleSeries }) => {
 			animate={{ opacity: 1 }}
 			transition={{ duration: 1 }}>
 			<div className="container relative mx-auto my-16 flex px-4">
-				<div className="flex flex-col gap-4 w-full">
+				<div className="flex w-full flex-col gap-4">
 					<div className="flex w-full justify-between">
-						<h2 className="text-white text-5xl">{serie.name}</h2>
+						<h2 className="text-5xl text-white">{serie.name}</h2>
 						<div className="flex flex-col gap-4">
-							<h5 className="text-slate-300 text-2xl">
+							<h5 className="text-2xl text-slate-300">
 								MovieMate <span className="uppercase">rating</span>
 							</h5>
 							<div className="flex items-center gap-4">
 								<StarIcon boxSize={'10'} color={'yellow.400'} />
-								<div className="flex text-white text-3xl">
+								<div className="flex text-3xl text-white">
 									<h3 className="font-semibold">
 										{serie.vote_average.toFixed(1)}
 									</h3>
@@ -37,20 +37,20 @@ const Serie = ({ singleSeries }) => {
 							</div>
 						</div>
 					</div>
-					<h5 className="text-white text-xl">{serie.first_air_date.slice(0, 4)}</h5>
-					<div className="flex gap-12">
+					<h5 className="text-xl text-white">{serie.first_air_date.slice(0, 4)}</h5>
+					<div className="flex flex-col gap-12 lg:flex-row">
 						<img
 							src={`https://image.tmdb.org/t/p/w500${serie.poster_path}`}
 							alt=""
-							className="rounded-md max-w-[24rem]"
+							className="max-w-[24rem] rounded-md"
 						/>
-						<div className="ml-auto flex flex-col gap-12 justify-between">
+						<div className="ml-auto flex flex-col justify-between gap-12">
 							<img
 								src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
 								alt=""
 								className="ml-auto"
 							/>
-							<p className="text-white text-2xl leading-10 text-center border-2 border-slate-400 bg-[#272727] p-6 rounded-xl">
+							<p className="rounded-xl border-2 border-slate-400 bg-[#272727] p-6 text-center text-2xl leading-10 text-white">
 								{serie.overview}
 							</p>
 						</div>
@@ -59,12 +59,13 @@ const Serie = ({ singleSeries }) => {
 			</div>
 			<div className="container mx-auto p-4">
 				<Box w={'full'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
-					<Link to={'/'} className='ml-auto'>
+					<Link to={'/'} className="ml-auto">
 						<Button
-							colorScheme="green"						
+							colorScheme="green"
 							fontSize={'xl'}
 							p={'6'}
-							leftIcon={<ArrowBackIcon />} onClick={() => document.querySelector('#search-bar').value = ''}>
+							leftIcon={<ArrowBackIcon />}
+							onClick={() => (document.querySelector('#search-bar').value = '')}>
 							Go to home page
 						</Button>
 					</Link>
